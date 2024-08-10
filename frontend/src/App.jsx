90ba2149e525e5e6ae4f98343e3ce817856d4b13
jayak <kumarijaya127@gmail.com>
2024-08-10 15:11:56 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>This is my noteapp</h1>
+      <Header />
+    </>
+  );
+}
+
+export default App;
